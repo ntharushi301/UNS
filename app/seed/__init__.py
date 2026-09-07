0aea5e3e18b9751c6e03b2df938d890c6457a59a
udxna <udanasrimal123@gmail.com>
2026-09-08 01:54:50 +0530

    Organize backend into backend directory

--- app/seed/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.seed.organization import seed_organization
-
-
-def seed_database():
-    seed_organization()
--- app/seed/run.py
+++
@@ -1,9 +0,0 @@
-from app import create_app
-from app.seed import seed_database
-
-
-app = create_app()
-
-
-with app.app_context():
-    seed_database()
